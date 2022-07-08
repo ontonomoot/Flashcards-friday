4127351325054b1a0e813252edb51188ba5828a4
Marie E <marrrielle.e@gmail.com>
2022-07-08 11:44:32 +0300

    release 0

--- Model.js
+++ Model.js
@@ -1,7 +1,48 @@
+const { rejects } = require('assert')
+const fs = require('fs')
+const { resolve } = require('path')
+
 class Model {
   constructor() {
 
   }
+  readTopics(){
+    fs.readdir('./topics', (err, nameFile) => {
+      if(err) {
+        return 'ошибка!'
+      } 
+      return nameFile
+    })
+  }
 }
+
+
+// readTopics = (path) => {
+//   return new Promise((resolve, rejects) =>{
+//     fs.readdir(path, (err, nameFile) =>{
+//       if(err) {
+//         rejects(err)
+//       } resolve(nameFile)
+//     })
+//   })
+// }
+
+// reName = (oldName, newName) => {
+//   return new Promise((resolve, rejects) => {
+//    fs.rename(oldName, newName, (err) => {
+//     if(err) {
+//       rejects(err)
+//     } resolve()
+//    })
+//   })
+// }
+
+// readTopics(path)
+//   .then(arr => {
+//     arr.forEach(name => {
+//       reName(`./topics/${name}`)
+//       .then(files => console.log(files))
+//     });
+//   })
 
 module.exports = Model
