27be3b1f997e936baa05b59b2854d575e375aafb
Anton Gorozhankin <obrv.igl@gmail.com>
2022-07-08 13:25:11 +0300

    Release 1

--- Controller.js
+++ Controller.js
@@ -1,6 +1,8 @@
 const readline = require ('readline')
 const { stdin : input, stdout: output } = require ('process');
 const rl = readline.createInterface ({input, output});
+const Model = require("./Model");
+const model = new Model
 
 class Controller {
   constructor(model, view) {
@@ -9,44 +11,47 @@ class Controller {
   }
 
   run() {
+
+    console.log(model.this);
+
     // Просим экземпляр класса модели прочитать папку со всеми темами и составить меню.
     // Попутно передаем метод контроллера this.printTopicsController,
     // так как нам нужно отправить сформинованное меню на вывод в экземпляр класса view
     // после того, как завершится асинхронная операция чтения папки
     // Здесь this.printTopicsController — является callback'ом  
-    this.model.readTopics(this.printTopicsController)
+    // this.model.readTopics(this.printTopicsController)
   }
 
-  printTopicsController(topicsMenu) {
-
-
+  printTopicsController() {
+    // console.log(this.model);
+  }
     
     // Тут нужно попросить экземпляр класса view вывести меню пользователю, 
     // а также дождаться ответа последнего
   }
 
   
-}
-const question = (question) =>
-  new Promise((resolve, reject) => 
-    rl.question(question ,(answer) => {
-      resolve (answer);
-    })
-  );
+// }
+// const question = (question) =>
+//   new Promise((resolve, reject) => 
+//     rl.question(question ,(answer) => {
+//       resolve (answer);
+//     })
+//   );
 
-  question ('Выбери тему: ' )
-    .then((data) => {
-      console.log('Твой ответ: ', data);
-      return question('Вопрос 1: ');
-    })
-    .then ((data) => {
-      console.log('\n', data)
-      return question('Вопрос 2: ');
-    })
-    .then ((data) => {
-      console.log('\n', data);
-    })
-    .then(() => rl.close())
+//   question ('Выбери тему: ' )
+//     .then((data) => {
+//       console.log('Твой ответ: ', data);
+//       return question('Вопрос 1: ');
+//     })
+//     .then ((data) => {
+//       console.log('\n', data)
+//       return question('Вопрос 2: ');
+//     })
+//     .then ((data) => {
+//       console.log('\n', data);
+//     })
+//     .then(() => rl.close())
 
 
 module.exports = Controller
