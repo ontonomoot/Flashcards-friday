f4f4a2457820ec2e129df5c11b91c9effba2736a
Marie E <marrrielle.e@gmail.com>
2022-07-08 12:50:13 +0300

    release 1

--- Model.js
+++ Model.js
@@ -15,42 +15,42 @@ class Model {
       for(let i = 0; i< a.length; i += 2){
         arr.push ({quest : a[i], answer : a[i + 1]})
       }
-      console.log(arr);
+      return arr;
       })
       
   }
-}
+  readTopics2(){
     
+    fs.readFile('./topics/otter_flashcard_data.txt', 'utf-8', (err, file) => {
+      const newArr = file.split('\n')
+      const a = newArr.filter((el) => el !== '')
+      let arr = [];
+      for(let i = 0; i< a.length; i += 2){
+        arr.push ({quest : a[i], answer : a[i + 1]})
+      }
+      return arr;
+      })
+      
+  }
+  readTopics3(){
+    
+    fs.readFile('./topics/raccoon_flashcard_data.txt', 'utf-8', (err, file) => {
+      const newArr = file.split('\n')
+      const a = newArr.filter((el) => el !== '')
+      let arr = [];
+      for(let i = 0; i< a.length; i += 2){
+        arr.push ({quest : a[i], answer : a[i + 1]})
+      }
+      return arr;
+      })
+      
+  }
 
+}
 
 const model = new Model()
-console.log(model.readTopics());
-// readTopics = (path) => {
-//   return new Promise((resolve, rejects) =>{
-//     fs.readdir(path, (err, nameFile) =>{
-//       if(err) {
-//         rejects(err)
-//       } resolve(nameFile)
-//     })
-//   })
-// }
-
-// reName = (oldName, newName) => {
-//   return new Promise((resolve, rejects) => {
-//    fs.rename(oldName, newName, (err) => {
-//     if(err) {
-//       rejects(err)
-//     } resolve()
-//    })
-//   })
-// }
-
-// readTopics(path)
-//   .then(arr => {
-//     arr.forEach(name => {
-//       reName(`./topics/${name}`)
-//       .then(files => console.log(files))
-//     });
-//   })
+model.readTopics()
+model.readTopics2()
+model.readTopics3()
 
 module.exports = Model
